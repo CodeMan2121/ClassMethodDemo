797be82e25e6d5633fd675fe9b97affcbd14ebb3
Administrator <Administrator@KINGHAZE>
2022-02-04 21:28:15 +0300

    Proje dosyası ekle.

---
+++ MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Adı: "+musteri.Name + "\n Müşteri ID: "+ musteri.ID);
+        }
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Name + " " + musteri.Surname);
+            }
+
+        }
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Name+" "+musteri.Surname + " elemanı silindi!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.ID = 1;
+            musteri1.Name = "Cebrail";
+            musteri1.Surname = "Meşe";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.ID = 2;
+            musteri2.Name = "Mikail";
+            musteri2.Surname = "Aslan";
+
+
+            Musteri[] musteriler = new Musteri[] {musteri1, musteri2};
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Listele(musteriler);
+            musteriManager.Silme(musteri2);
+        }
+
+        
+    }
+}
